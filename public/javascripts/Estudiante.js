3987e7edf50e468e5a7cbe4e0e4a0914143ccd91
dmaflab <danielmaflab@hotmail.com>
2018-04-10 08:28:21 -0500

    cambio de rutas

--- public/javascripts/Estudiante.js
+++ public/javascripts/Estudiante.js
@@ -1,28 +1,28 @@
-$(function(){
+$(function() {
 
-//función ajax eliminar producto
+    //función ajax eliminar producto
 
-$('#tbl-estudiantes #btn-eliminar').click(function(e){
- 	e.preventDefault();
- 	var elemento= $(this);
- 	var id = elemento.parent().parent().find('#id').text();
- 	var confirmar= confirm('desea eliminar el estudiante');
- 	if(confirmar){
- 		
- 	
- 	$.ajax({
- 		url : 'http://localhost:3000/eliminarestudiante',
- 		method : 'post',
- 		data : {id : id},
- 		success :  function(res){
- 			if(res.res){
- 				elemento.parent().parent().remove();
- 			}
- 		}
- 		
- 	});
-}
+    $('#tbl-estudiantes #btn-eliminar').click(function(e) {
+        e.preventDefault();
+        var elemento = $(this);
+        var id = elemento.parent().parent().find('#id').text();
+        var confirmar = confirm('desea eliminar el estudiante');
+        if (confirmar) {
 
-});
+
+            $.ajax({
+                url: 'http://colegio-teamcrudgoogle.7e14.starter-us-west-2.openshiftapps.com/eliminarestudiante',
+                method: 'post',
+                data: { id: id },
+                success: function(res) {
+                    if (res.res) {
+                        elemento.parent().parent().remove();
+                    }
+                }
+
+            });
+        }
+
+    });
 
 });
--- public/javascripts/calificacion.js
+++ public/javascripts/calificacion.js
@@ -1,28 +1,28 @@
-$(function(){
+$(function() {
 
-//función ajax eliminar producto
+    //función ajax eliminar producto
 
-$('#tbl-calificaciones #btn-eliminar').click(function(e){
- 	e.preventDefault();
- 	var elemento= $(this);
- 	var id = elemento.parent().parent().find('#id').text();
- 	var confirmar= confirm('desea eliminar la calificacion');
- 	if(confirmar){
- 		
- 	
- 	$.ajax({
- 		url : 'http://localhost:3000/eliminarcalificacion',
- 		method : 'post',
- 		data : {id : id},
- 		success :  function(res){
- 			if(res.res){
- 				elemento.parent().parent().remove();
- 			}
- 		}
- 		
- 	});
-}
+    $('#tbl-calificaciones #btn-eliminar').click(function(e) {
+        e.preventDefault();
+        var elemento = $(this);
+        var id = elemento.parent().parent().find('#id').text();
+        var confirmar = confirm('desea eliminar la calificacion');
+        if (confirmar) {
 
-});
+
+            $.ajax({
+                url: 'http://colegio-teamcrudgoogle.7e14.starter-us-west-2.openshiftapps.com/eliminarcalificacion',
+                method: 'post',
+                data: { id: id },
+                success: function(res) {
+                    if (res.res) {
+                        elemento.parent().parent().remove();
+                    }
+                }
+
+            });
+        }
+
+    });
 
 });
